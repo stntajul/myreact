ccb5cbb857fcb8e68cef127cf47783cb1661df05
stntajul <tajul.stn99@gmail.com>
2021-11-09 11:59:36 +0600

    event handler added

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,26 +1,47 @@
 import React from 'react';
 
 class Clock extends React.Component {
-    constructor(props) {
+    // use props and constructor if we need to use props
+    /* constructor(props) {
         super(props);
         this.state = { date: new Date() };
-    }
+    } */
+    state = { date: new Date(), locale: 'bn-BD' }; // if use props the we need to use previous code
 
     componentDidMount() {
-        setInterval(() => {
-            this.setState({ date: new Date() });
+        this.clockTimer = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.clockTimer);
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    handleClick = () => {
+        // console.log('button was clicked');
+        this.setState({ locale: 'en-US' });
+    };
+
+    tick() {
+        this.setState({
+            date: new Date(),
         });
     }
 
     render() {
         this.t = new Date();
-        const { locale } = this.props;
-        const { date } = this.state;
+
+        const { date, locale } = this.state;
 
         return (
-            <h1 className="heading">
-                <span className="text">{date.toLocaleTimeString(locale)}</span>
-            </h1>
+            <div>
+                <h1 className="heading">
+                    <span className="text">{date.toLocaleTimeString(locale)}</span>
+                </h1>
+                <button type="submit" onClick={this.handleClick}>
+                    Click Me
+                </button>
+            </div>
         );
     }
 }
